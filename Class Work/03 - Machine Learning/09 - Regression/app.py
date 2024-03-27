9a16d09974b8e701d519d2dcbef047b99d5826e6
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-03-27 10:08:54 +0530

    27-3-2024

--- Class Work/03 - Machine Learning/09 - Regression/app.py
+++ Class Work/03 - Machine Learning/09 - Regression/app.py
@@ -12,5 +12,25 @@ scaler=pkl.load(open('models/scaler2.pkl','rb'))
 def index():
     return render_template('index.html')
 
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='POST':
+        temp=float(request.form.get('Temperature'))
+        rh=float(request.form.get('RH'))
+        ws=float(request.form.get('Ws'))
+        rain=float(request.form.get('Rain'))
+        ffmc=float(request.form.get('FFMC'))
+        dmc=float(request.form.get('DMC'))
+        isi=float(request.form.get('ISI'))
+        classes=float(request.form.get('Classes'))
+        reg=float(request.form.get('Region'))
+
+        new_scaled_data=scaler.transform([[temp,rh,ws,rain,ffmc,dmc,isi,classes,reg]])
+        result=ridge.predict(new_scaled_data)
+
+        return render_template('home.html',result=result[0])
+    else:
+        return render_template('home.html')
+
 if __name__=='__main__':
-    app.run(host='localhost',port=5000)
+    app.run(host='localhost',port=5000,debug=True)
